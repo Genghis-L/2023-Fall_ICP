29d36695530e5dd69a3c0eb65fe447794956c121
Genghis Luo <kl4747@nyu.edu>
2023-09-08 23:10:57 +0800

    HW2

--- exercise1.py
+++ exercise1.py
@@ -1,17 +1,23 @@
 phase = input("Is the moon full? If yes enter Full if no enter No: ")
 distance = int(input("Enter the distance of the moon from the earth: "))
 date = int(input("Enter the date of the month: "))
-eclipse = bool(input("Is there an eclipse? Enter True for yes and False for no: "))
+eclipse = input("Is there an eclipse? Enter True for yes and False for no: ")
+
+# We need to make eclipse a boolean-value variable
+if eclipse == "True":
+    eclipse = True
+else:
+    eclipse = False
 
 flag = 0  # A flag to check whether it is special or not
 
 if phase == "No":
     print("Moon")
 else:
-    if distance < 230000: # To check whether it is Super
+    if distance < 230000:  # To check whether it is Super
         print("Super", end=" ")
         flag = 1
-    if date == 29 or 30 or 31: # To check whether it is Blue
+    if date == (29 or 30 or 31):  # To check whether it is Blue
         print("Blue", end=" ")
         flag = 1
     if eclipse == True:  # To check whether it is Blood
@@ -19,5 +25,5 @@ else:
         flag = 1
     if flag == 1:
         print("Moon")
-    else: # falg==0 means it is not special
+    else:  # falg==0 means it is not special
         print("Full Moon")
