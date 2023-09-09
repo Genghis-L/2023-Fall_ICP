cca1b92703e9f93053a1626705c922ff5237b71b
Genghis Luo <kl4747@nyu.edu>
2023-09-09 14:05:01 +0800

    Revision for HW1

--- Homework 1/exercise1.py
+++ Homework 1/exercise1.py
@@ -1,3 +1,6 @@
 fname = input("What is your family name: ")
 gname = input("What is your given name: ")
-print(f"Hello {gname} {fname} !!!")
+print("Hello", gname, fname, "!!!")
+
+# or we can use fstring:
+# print(f"Hello {gname} {fname} !!!")
--- Homework 1/exercise2.py
+++ Homework 1/exercise2.py
@@ -1,13 +1,17 @@
 grade1 = int(input("1st grade: "))
 grade2 = int(input("2nd grade: "))
 grade3 = int(input("3rd grade: "))
-Average = round((grade1 + grade2 + grade3) / 3)
+Average = (grade1 + grade2 + grade3) / 3
+
 """
     Solution 1: Using round function
     Average = round((grade1 + grade2 + grade3) / 3)
 """
-# Solution 2: Using "if" to check the decimal part
+
+# Solution 2: Using "if" to do rounding
 if (Average * 10) % 10 >= 5:
-    Average += int(Average)
+    Average = int(Average) + 1
+else:
+    Average = int(Average)
 
 print(f"Average grade: {Average}")
