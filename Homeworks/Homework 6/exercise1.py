78b1367767023ec609cdd9caf94373d45f779ee8
Genghis Luo <kl4747@nyu.edu>
2023-11-05 15:41:27 +0800

    HW6

--- Homeworks/Homework 6/exercise1.py
+++ Homeworks/Homework 6/exercise1.py
@@ -4,6 +4,7 @@
 # See instructions in pdf file
 #
 
+
 def main():
     """The program does the following:
     1) asks the user to input 5 usernames (only alphanumeric characters will be input,
@@ -12,24 +13,32 @@ def main():
     3) calls clean_users function to clean the list
     4) prints out the cleaned list"""
     # WRITE YOUR CODE BELOW
-    
-    
+    L = []
+    for k in range(5):
+        L.append(input("username: "))
+    print(L)
+    print(clean_users(L))
 
 
 def clean_users(L):
     """• input: 1 parameter L -> type list (of strings) - each element in the list is a username
-• return: a new cleaned list (see the rules below) -> type list (of strings)
+    • return: a new cleaned list (see the rules below) -> type list (of strings)
 
-The logic for cleaning the list of usernames is:
-    • if the username contains c, g or z, it should be removed (the same with C, G or Z)
-    • otherwise, keep it in the list
+    The logic for cleaning the list of usernames is:
+        • if the username contains c, g or z, it should be removed (the same with C, G or Z)
+        • otherwise, keep it in the list
 
-Note: your function should not modify the original list!"""
+    Note: your function should not modify the original list!"""
     # WRITE YOUR CODE BELOW
-    
-    
+    Cleaned_L = []
+    for v in L:
+        if "c" in v or "C" in v or "g" in v or "G" in v or "z" in v or "Z" in v:
+            continue
+        else:
+            Cleaned_L.append(v)
+    return Cleaned_L
 
 
-#Call the main() function, DO NOT CHANGE THE CODE BELLOW
-if __name__ == '__main__':
+# Call the main() function, DO NOT CHANGE THE CODE BELLOW
+if __name__ == "__main__":
     main()
--- Homeworks/Homework 6/exercise2.py
+++ Homeworks/Homework 6/exercise2.py
@@ -17,6 +17,21 @@ def valid_username(user, L):
     2. Returns if the username is valid → type bool
     """
     # WRITE YOUR CODE BELOW
+    if (
+        len(user) >= 4
+        and user.isalnum()
+        and user[0].isalpha()
+        and not any(user in sublist for sublist in L)
+    ):
+        print("Valid")
+        return True
+    elif any(user in sublist for sublist in L):
+        print("User Name Exists")
+        return False
+    else:
+        print("Invalid")
+        return False
+
 
 def valid_password(pwd):
     """
@@ -24,6 +39,19 @@ def valid_password(pwd):
     2. Returns if the password is valid → type bool
     """
     # WRITE YOUR CODE BELOW
+    if (
+        len(pwd) >= 10
+        and pwd.isalnum()
+        and pwd != pwd.upper()
+        and pwd != pwd.lower()
+        and not pwd.isalpha()
+    ):
+        print("Valid")
+        return True
+    else:
+        print("Invalid")
+        return False
+
 
 def add_user(L):
     """
@@ -36,13 +64,33 @@ def add_user(L):
     3. Add the username/password pair to the userList and display User created
     """
     # WRITE YOUR CODE BELOW
+    while True:
+        user = input("Enter Username: ")
+        if not valid_username(user, L):
+            continue
+        else:
+            while True:
+                pwd = input("Enter Password: ")
+                if not valid_password(pwd):
+                    continue
+                else:
+                    pwd_a = input("Confirm Password: ")
+                    if pwd_a != pwd:
+                        print("Passwords do not match")
+                        continue
+                    else:
+                        print("User created")
+                        return L.append([user, pwd])
 
 
-    
 # DO NOT CHANGE THE CODE BELLOW
-if __name__ == '__main__':
-    userList = [['sunny1', 'pwd1DdeEff'], ['superS', 'pwD2Abcdefgh'],
-    ['likeA', 'pwd3AAAAAA'], ['qwerty', 'pwd4QWERTY']]
+if __name__ == "__main__":
+    userList = [
+        ["sunny1", "pwd1DdeEff"],
+        ["superS", "pwD2Abcdefgh"],
+        ["likeA", "pwd3AAAAAA"],
+        ["qwerty", "pwd4QWERTY"],
+    ]
 
     add_user(userList)
 
